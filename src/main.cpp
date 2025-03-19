075e0f47f4d625775604dbe0e5ac1b48de1e9135
Brandon Mercado <brandon.mercado.264@my.csun.edu>
2025-03-19 16:25:34 -0700

    prompts user to enter options for task handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,37 @@
-#include <iostream>
 #include "opts.cpp"
 #include "tasks/task.hpp"
+#include <cctype>
+#include <cstdlib>
+#include <iostream>
 using namespace std;
 
 int main() {
-
   char option;
+  string empty;
 
-  /* while (true) {
-    
-  } */
-  print_opts(options);
+  cout << "-----WELCOME TO YOUR TASKS-----" << endl
+       << "    CLICK ENTER TO CONTINUE " << endl;
+
+  // Handles "enter" key to continue
+  getline(cin, empty);
+
+  // "Game" loop to keep program going until user quits
+  while (true) {
+    system("clear");
+    print_opts(options);
+
+    // Prompt user for their input and capitalize it
+    cout << endl << "What do you want to do?: ";
+    cin >> option;
+    option = toupper(option);
+
+    // HANDLE USER QUIT OUTSIDE OF SWITCH CASE
+    if (option == 'Q') {
+      cout << "Exiting program..." << endl << "Goodbye!" << endl;
+      break;
+    }
+
+    // switch (option) {}
+  }
   return 0;
 }
--- src/opts.cpp
+++ src/opts.cpp
@@ -6,16 +6,16 @@
 using namespace std;
 
 vector<vector<string>> options = {
-  {"A", "ADD"},
-  {"U", "UPDATE"},
-  {"D", "DELETE"},
+  {"A", "ADD TASK"},
+  {"U", "UPDATE TASK"},
+  {"D", "DELETE TASK"},
   {"Q", "QUIT"}
 };
 
 void print_opts(vector<vector<string>> &t) {
 
   auto lam = [](vector<string> &s) {
-    cout << s[0] << ": " << s[1] << endl;
+    cout << "- " << s[0] << ": " << s[1] << endl;
   };
 
   for_each(t.begin(), t.end(), lam);
